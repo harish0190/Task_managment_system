99f00495b183498bc8a7c066f8bf587339e4d30a
Harish <hgoraniya568@gmail.com>
2026-03-20 12:30:46 +0530

    Fix data.sql duplicate entry bug

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 -- Default Users
 -- Passwords are BCrypt hashes of 'password123'
-INSERT INTO users (username, password, role) VALUES ('manager', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_MANAGER');
-INSERT INTO users (username, password, role) VALUES ('employee', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+INSERT IGNORE INTO users (username, password, role) VALUES ('manager', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_MANAGER');
+INSERT IGNORE INTO users (username, password, role) VALUES ('employee', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
