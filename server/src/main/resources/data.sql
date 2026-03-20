2c58e9be968436b22b0e66f536f4304e48f5a38c
Harish <hgoraniya568@gmail.com>
2026-03-20 12:48:08 +0530

    Force-update passwords in data.sql to fix login bug

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Default Users
 -- Passwords are BCrypt hashes of 'password123'
-INSERT IGNORE INTO users (username, password, role) VALUES ('manager', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_MANAGER');
-INSERT IGNORE INTO users (username, password, role) VALUES ('employee', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
-INSERT IGNORE INTO users (username, password, role) VALUES ('harish_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
-INSERT IGNORE INTO users (username, password, role) VALUES ('test_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
-INSERT IGNORE INTO users (username, password, role) VALUES ('Deep', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+REPLACE INTO users (username, password, role) VALUES ('manager', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_MANAGER');
+REPLACE INTO users (username, password, role) VALUES ('employee', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+REPLACE INTO users (username, password, role) VALUES ('harish_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+REPLACE INTO users (username, password, role) VALUES ('test_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+REPLACE INTO users (username, password, role) VALUES ('Deep', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
