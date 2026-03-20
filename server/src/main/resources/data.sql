1c053410e44872af29bf97fc1ce59d6261723e2d
Harish <hgoraniya568@gmail.com>
2026-03-20 12:36:40 +0530

    Add many default employee accounts for testing

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,7 +1,6 @@
--- Migrate legacy roles if they exist
-UPDATE users SET role = 'ROLE_EMPLOYEE' WHERE role = 'ROLE_MEMBER';
-
 -- Default Users
 -- Passwords are BCrypt hashes of 'password123'
 INSERT IGNORE INTO users (username, password, role) VALUES ('manager', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_MANAGER');
 INSERT IGNORE INTO users (username, password, role) VALUES ('employee', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+INSERT IGNORE INTO users (username, password, role) VALUES ('harish_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+INSERT IGNORE INTO users (username, password, role) VALUES ('test_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
