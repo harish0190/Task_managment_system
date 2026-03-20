fc587d61b0c90872ec260ab345baa84c608702f6
Harish <hgoraniya568@gmail.com>
2026-03-20 12:46:24 +0530

    Add employee 'Deep' to default users

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -4,3 +4,4 @@ INSERT IGNORE INTO users (username, password, role) VALUES ('manager', '$2a$10$8
 INSERT IGNORE INTO users (username, password, role) VALUES ('employee', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
 INSERT IGNORE INTO users (username, password, role) VALUES ('harish_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
 INSERT IGNORE INTO users (username, password, role) VALUES ('test_emp', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
+INSERT IGNORE INTO users (username, password, role) VALUES ('Deep', '$2a$10$8.UnVuG9shgD3W9tfJUXLuE4FahpFYMxngbcNoY8BIsCfayO4w8vO', 'ROLE_EMPLOYEE');
